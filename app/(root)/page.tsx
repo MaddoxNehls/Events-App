3c4ad88401a32d4e6987a04aa54aebf2cc245486
MaddoxNehls <mnehls0645@gmail.com>
2023-12-15 23:06:12 -0800

    finishing the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
-//import CategoryFilter from '@/components/shared/CategoryFilter';
+import CategoryFilter from '@/components/shared/CategoryFilter';
 import Collection from '@/components/shared/Collection'
-//import Search from '@/components/shared/Search';
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
@@ -47,8 +47,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
 
         <Collection 
